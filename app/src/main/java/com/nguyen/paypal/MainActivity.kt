a292186a17fde9970c6742bcd2f27ec014516dc8
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2024-11-04 17:01:16 -0800

    replaced RecyclerView with ViewPager2 in SimilarFragment

--- app/src/main/java/com/nguyen/paypal/MainActivity.kt
+++ app/src/main/java/com/nguyen/paypal/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.nguyen.paypal
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.nguyen.paypal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/nguyen/paypal/SimilarFragment.kt
+++ app/src/main/java/com/nguyen/paypal/SimilarFragment.kt
@@ -1,16 +1,11 @@
 package com.nguyen.paypal
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.nguyen.paypal.databinding.FragmentSimilarBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 private const val TAG = "Truong-SimilarFragment"
 class SimilarFragment : Fragment(R.layout.fragment_similar) {
@@ -19,23 +14,10 @@ class SimilarFragment : Fragment(R.layout.fragment_similar) {
 
         val movies = mutableListOf<Movie>()
         val similarAdapter = SimilarAdapter(movies)
-        FragmentSimilarBinding.bind(view).recycler.apply {
-            adapter = similarAdapter
-            layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
-        }
+        FragmentSimilarBinding.bind(view).viewpager.adapter = similarAdapter
 
         val args: SimilarFragmentArgs by navArgs()
         val viewModel : SimilarViewModel by viewModels()
-        Network.service.getSimilar2(args.movieId, "1fca74d1a066b2433a06dea9b96239fe").enqueue(object: Callback<Movies> {
-            override fun onResponse(p0: Call<Movies>, p1: Response<Movies>) {
-                Log.d(TAG, "onResponse $p1")
-            }
-
-            override fun onFailure(p0: Call<Movies>, p1: Throwable) {
-                Log.d(TAG, "onFailure $p1")
-            }
-
-        })
         viewModel.getSimilar(args.movieId)
         viewModel.movies.observe(requireActivity()) {
             movies.clear()
